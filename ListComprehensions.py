6542bc442b75fda497a1da11f5994a96b88b0fa0
SnehaShree2501 <snehashree2501@gmail.com>
2020-12-18 23:34:04 +0530

    added comments

--- ListComprehensions.py
+++ ListComprehensions.py
@@ -3,8 +3,8 @@ y = int(input("Enter 2nd co-ordinate"))
 z = int(input("Enter 3rd ordinate"))
 n = int(input("Enter the limit"))
 ListOfNumbers =[]
-ListOfNumbers = [[i,j,k] 
-for i in range(0,x+1)
+ListOfNumbers = [[i,j,k] #list for the loop control variables
+for i in range(0,x+1) #multiple loops running here
 for j in range(0,y+1)
 for k in range(0,z+1)
 if i+j+k!=n ]
